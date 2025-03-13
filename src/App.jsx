55e902dc13c471d8bfd3003cb633489f287c9ad5
Agostina <agosqdo@gmail.com>
2025-03-12 23:12:59 -0300

    Updated modified files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Home from "./pages/Home";
+import { Analytics } from "@vercel/analytics/react"; 
 import "./styles/styles.css";
 
 function App() {
   return (
     <div className="app">
       <Home />
+      <Analytics /> 
     </div>
   );
 }
