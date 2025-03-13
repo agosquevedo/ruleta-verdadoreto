3bdf4eb53706c7af5c25dca13ef970c304fb22c9
agosquevedo <agosqdo@gmail.com>
2025-03-13 11:30:12 -0300

    Updated ChallengeCard component

--- src/components/ChallengeCard.jsx
+++ src/components/ChallengeCard.jsx
@@ -10,17 +10,18 @@ const ChallengeCard = ({ challenge, setSelectedChallenge }) => {
   
   return (
     <motion.div 
-      initial={{ scale: 0 }}
-      animate={{ scale: 1 }}
-      exit={{ scale: 0 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.3, ease: "easeOut" }}
       className="modal_container">
       
       <div className="modal">
         <div className="modal_header">
           <motion.h1
-            initial={{ opacity: 0, x: 100 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ delay: 0.5 }}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3 }}
           >
             {challenge.level} {challenge.emoji}
           </motion.h1>
